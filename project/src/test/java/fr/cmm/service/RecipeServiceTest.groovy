234c0a68bfb9ff201039e52e243c0c3ba2e2351f
pomme <clement.dechy@gmail.com>
2015-12-18 17:19:49 +0100

    Test en Groovy

--- project/src/test/java/fr/cmm/service/RecipeServiceTest.groovy
+++ project/src/test/java/fr/cmm/service/RecipeServiceTest.groovy
@@ -42,7 +42,7 @@ public class RecipeServiceTest {
         recipeCollection.remove();
     }
 
-    @Test
+    /*@Test
     public void save() {
         Recipe recipe = new Recipe();
         recipe.setTitle("test recipe");
@@ -50,9 +50,15 @@ public class RecipeServiceTest {
         recipeService.save(recipe);
 
         assertEquals("test recipe", recipeCollection.findOne().as(Recipe.class).getTitle());
-    }
+    }*/
 
     @Test
+    void save() {
+        recipeService.save(new Recipe(title: 'test recipe'))
+        assert recipeCollection.findOne().as(Recipe).title == 'test recipe'
+    }
+
+    /*@Test
     public void findById() {
         Recipe recipe = new Recipe();
         recipe.setTitle("test recipe");
@@ -60,9 +66,15 @@ public class RecipeServiceTest {
         recipeService.save(recipe);
 
         assertEquals("test recipe", recipeService.findById(recipe.getId()).getTitle());
-    }
+    }*/
 
     @Test
+    void findById(){
+        recipeService.save(new Recipe (title : 'test recipe', id : '56728b44300416fb9c3fa05d'))
+        assert recipeService.findById('56728b44300416fb9c3fa05d').title == 'test recipe'
+    }
+
+    /*@Test
     public void findByQuery() {
         recipeService.save(new Recipe());
         recipeService.save(new Recipe());
@@ -71,9 +83,16 @@ public class RecipeServiceTest {
         recipeService.save(new Recipe());
 
         assertEquals(5, stream(recipeService.findByQuery(new PageQuery()).spliterator(), false).count());
-    }
+    }*/
 
     @Test
+    void findByQuery(){
+        5.times{recipeService.save(new Recipe())}
+        assert stream(recipeService.findByQuery(new PageQuery()).spliterator(), false).count() == 5
+    }
+
+
+    /*@Test
     public void findByQueryWithCustomPageSize() {
         recipeService.save(new Recipe());
         recipeService.save(new Recipe());
@@ -85,6 +104,12 @@ public class RecipeServiceTest {
         pageQuery.setSize(2);
 
         assertEquals(2, stream(recipeService.findByQuery(pageQuery).spliterator(), false).count());
+    }*/
+
+    @Test
+    void findByQueryWithCustomPageSize(){
+        5.times{recipeService.save(new Recipe())}
+        stream(recipeService.findByQuery(new PageQuery('size' : 2)).spliterator(), false).count() == 2
     }
 
     @Test
